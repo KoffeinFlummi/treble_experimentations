7b7b4ddbcd08e15108b56a544f3b1b547bdc38b0
KoffeinFlummi <koffeinflummi@gmail.com>
2018-08-09 19:27:36 +0200

    Update build-dakkar to r43

--- build-dakkar.sh
+++ build-dakkar.sh
@@ -79,7 +79,7 @@ function get_rom_type() {
                 ;;
             aosp81)
                 mainrepo="https://android.googlesource.com/platform/manifest"
-                mainbranch="android-8.1.0_r36"
+                mainbranch="android-8.1.0_r43"
                 localManifestBranch="android-8.1"
                 treble_generate=""
                 extra_make_options=""
